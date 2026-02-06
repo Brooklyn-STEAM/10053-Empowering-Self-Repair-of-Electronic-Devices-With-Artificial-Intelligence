a25982c579880c3400013b8440d9537b53b780b5
Caleb Ogboe <cogboe@brooklynsteamcenter.org>
2026-02-06 09:16:57 -0500

    Flask_set_up

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, flash, redirect, abort, session
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+import pymysql
+from dynaconf import Dynaconf
+
+
+app = Flask(__name__)
+
+config = Dynaconf(settings_file =["settings.toml"])
+
+app.secret_key = config.secret_key
+
+login_manager = LoginManager(app)
+
+
+class User:
+    is_authenticated = True
+    is_active = True
+    is_anonymous = False
+
+    def __init__(self, result):
+        self.name = result["Name"]
+        self.email = result["Email"]
+        self.address = result["Address"]
+        self.id = result["ID"]
+
+    def get_id(self):
+        return str(self.id)
+
+
+@app.route("/")
+def index():
+    return render_template("homepage.html.jinja")
