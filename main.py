abfa9c037b798175a2c0e3d8a02f6a2162c6c163
Jayson Ramirez <jramierz@brooklynsteamcenter.or>
2026-02-13 09:24:39 -0500

    .

--- main.py
+++ main.py
@@ -60,10 +60,6 @@ def connect_db():
 def index():
     return render_template("homepage.html.jinja")
 
-@app.route("/about_us")
-def about_us():
-    return render_template("about_us.html.jinja")
-    return render_template("about_us.html.jinja")
 @app.route("/login", methods=["POST","GET"])
 def login_page():
     if request.method == 'POST':
