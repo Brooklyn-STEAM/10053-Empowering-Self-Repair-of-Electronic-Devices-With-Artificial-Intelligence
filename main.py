f7bfc83bb3e1a16b2b2abe504b4a3bf855fe949c
Jayson Ramirez <jramierz@brooklynsteamcenter.or>
2026-03-09 09:10:49 -0400

    now it should work

--- main.py
+++ main.py
@@ -234,29 +234,28 @@ def product_page(product_id):
         abort(404)
     return render_template("product.html.jinja", product=result , review=review)
 
-
 @app.route("/product/<int:product_id>/add_to_cart", methods=["POST"])
 @login_required
 def addtocart(product_id):
-    pass
+
     try:
         quantity = int(request.form["qty"])
         if quantity <= 0:
             raise ValueError
     except (KeyError, ValueError):
         flash("Invalid quantity")
-        return redirect(f"/product/{int(product_id)}")
+        return redirect(url_for("product", product_id=product_id))
 
     connection = connect_db()
     cursor = connection.cursor()
 
     cursor.execute("""
-        INSERT INTO `Cart` (`Quantity`, `ProductID`, `UserID`)
+        INSERT INTO Cart (Quantity, ProductID, UserID)
         VALUES (%s, %s, %s)
         ON DUPLICATE KEY UPDATE
-        `Quantity` = `Quantity` + VALUES(`Quantity`)
+        Quantity = Quantity + VALUES(Quantity)
     """, (quantity, product_id, current_user.id))
-    
+
     connection.commit()
     connection.close()
 
