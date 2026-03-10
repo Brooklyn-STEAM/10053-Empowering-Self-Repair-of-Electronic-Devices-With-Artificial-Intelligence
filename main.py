8c35765dce2c20440cd7fe72f03861d216b0d191
Caleb <cogboe@brooklynsteamcenter.org>
2026-03-10 10:15:17 -0400

    added-images-tweaked-search-results (#29)

--- main.py
+++ main.py
@@ -144,11 +144,6 @@ def about_us():
 def error():
     return render_template("404.html.jinja")
 
-@app.route("/cart")
-@login_required
-def cart():
-    return render_template("cart.html.jinja")
-
 @app.route("/search", methods=["GET"])
 def search():
     query = request.args.get("q", "").strip()
